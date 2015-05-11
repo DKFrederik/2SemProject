ff69b46c46bb6206827f3d7136dc66662b30dffd
Nichlas Lindorf Pedersen <Nichlaslp@fastmail.com>
2015-05-11 08:46:55 +0200

    Bday Date

--- 2SemProject/src/modelLayer/Player.java
+++ 2SemProject/src/modelLayer/Player.java
@@ -1,13 +1,16 @@
 package modelLayer;
 
+import java.sql.Date;
+import java.util.Calendar;
+
 public class Player extends Person{
 	
-	private int age;
+	private Date bDay;
 	private String position;
 	
-	public Player(String fname, String lname, String email, String phone, int age, String position) {
+	public Player(String fname, String lname, String email, String phone, Date bDay, String position) {
 		super(fname, lname, email, phone);
-		this.age = age;
+		this.bDay = bDay;
 		this.position = position;
 	}
 	
@@ -15,12 +18,12 @@ public class Player extends Person{
 		
 	}
 
-	public int getAge() {
-		return age;
+	public Date getBDay() {
+		return bDay;
 	}
 
-	public void setAge(int age) {
-		this.age = age;
+	public void setBDay(Date bDay) {
+		this.bDay = bDay;
 	}
 
 	public String getPosition() {
@@ -29,6 +32,18 @@ public class Player extends Person{
 
 	public void setPosition(String position) {
 		this.position = position;
+	}
+	
+	public int getAge() {
+		Calendar bday = Calendar.getInstance();
+		bday.setTime(bDay);
+		
+		Calendar today = Calendar.getInstance();
+		int age = today.get(Calendar.YEAR) - bday.get(Calendar.YEAR);
+		
+		if (today.get(Calendar.DAY_OF_YEAR) < bday.get(Calendar.DAY_OF_YEAR));
+			age--;
+		return age;
 	}
 
 }
