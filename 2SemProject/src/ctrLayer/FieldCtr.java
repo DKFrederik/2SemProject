8c49e441e11e5cb15ab57a3e393976b9b0028b78
DKFrederik <dkfrede@gmail.com>
2015-05-21 10:01:19 +0200

    updated FieldCtr

--- 2SemProject/src/ctrLayer/FieldCtr.java
+++ 2SemProject/src/ctrLayer/FieldCtr.java
@@ -7,47 +7,50 @@ import modelLayer.*;
 import dbLayer.*;
 
 public class FieldCtr {
-	
+
 	private static FieldCtr instance = null;
-	private DBField fDB; 
-	
-	public FieldCtr()
-	{
+	private DBField fDB;
+
+	public FieldCtr() {
 		fDB = new DBField();
 	}
-	
-	public static FieldCtr getInstance()
-	{
-		if(instance == null)
-		{
+
+	public static FieldCtr getInstance() {
+		if (instance == null) {
 			instance = new FieldCtr();
 		}
-		
+
 		return instance;
 	}
-	
-	public void insertField(Field f)
-	{
-		
+
+	public void insertField(Field f) {
+
 	}
-	
-	public Team findField(String fieldNumber)
-	{
-		return null;
+
+	/**
+	 * Finds a Field in the DB.
+	 * 
+	 * @param fieldNumber
+	 *            the number of the field.
+	 * @return the matching Field.
+	 */
+	public Field findField(String fieldNumber) {
+		return fDB.findField(fieldNumber);
 	}
-	
-	public void deleteField(String fieldNumber)
-	{
-		
+
+	public boolean deleteField(String fieldNumber) {
+		if (0 < fDB.deleteField(fieldNumber)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
-	public void updateField(Field f, String oldFieldNumber)
-	{
-		
+
+	public void updateField(Field f, String oldFieldNumber) {
+
 	}
-	
-	public List<Field> getFields()
-	{
+
+	public List<Field> getFields() {
 		return fDB.getAllFields(true);
 	}
 }
