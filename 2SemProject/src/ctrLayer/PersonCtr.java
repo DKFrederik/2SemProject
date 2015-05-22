4987b41511438fa0b0c7bcf339f47367f620eaa3
Nichlas Lindorf Pedersen <Nichlaslp@fastmail.com>
2015-05-22 13:43:24 +0200

    Added methods for updating various types of Persons

--- 2SemProject/src/ctrLayer/PersonCtr.java
+++ 2SemProject/src/ctrLayer/PersonCtr.java
@@ -1,17 +1,22 @@
 package ctrLayer;
 
+import modelLayer.Manager;
 import modelLayer.Person;
 import modelLayer.Player;
+import modelLayer.Referee;
+import modelLayer.Staff;
+import modelLayer.TeamLeader;
 import dbLayer.DBPerson;
 
 public class PersonCtr {
-	
+
 	private static PersonCtr instance = null;
 	private DBPerson dbP;
-	
+
 	private PersonCtr() {
 		dbP = new DBPerson();
 	}
+
 	public static PersonCtr getInstance() {
 		if (instance == null) {
 			instance = new PersonCtr();
@@ -20,33 +25,44 @@ public class PersonCtr {
 		return instance;
 	}
 	
-	public void createPlayer(String fname, String lname, String email, String phoneno, String zipcode, 
-			String sbDay, String position) throws Exception {
+	public boolean createManager(String fname, String lname, String email,
+			String phoneno, String zipcode, String username, String password, double salary)
+			throws Exception {
 
-		dbP.insertPerson(new Player(fname, lname, email, phoneno, zipcode, sbDay, position));
+		if(0 < dbP.insertPerson(new Manager(fname, lname, email, phoneno, zipcode, username, password, salary))) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
+	public boolean createPlayer(String fname, String lname, String email,
+			String phoneno, String zipcode, String sbDay, String position)
+			throws Exception {
+
+		if(0 < dbP.insertPerson(new Player(fname, lname, email, phoneno, zipcode,
+				sbDay, position))) {
+			return true;
+		}
+		else {
+			return false;
+		}
 
 	}
 	
+	public void 
+
 	public Person findPerson(String phoneno) {
 		return dbP.findPerson(phoneno, true);
 	}
 	
-	public boolean updatePerson(String fname, String lname, String email, String phoneno, String zipcode, 
-			String sbDay, String position, char type, String oldPhone) {
-		Person p = null;
-		if(type == 'T') {
-			//Teamleader
-		}
-		else if(type == 'M') {
-			//Manager
-		}
-		else if(type == 'S') {
-			//STAFF
-		}
-		else if(type == 'P') {
-			p = new Player(fname, lname, email, phoneno, zipcode, sbDay, position);
-		}
-		if(0 < dbP.updatePerson(p, oldPhone)) {
+	public boolean updateReferee(String fname, String lname, String email,
+			String phoneno, String zipcode, String oldPhone) {
+		Referee r = new Referee(fname, lname, email, phoneno, zipcode);
+		
+		if(0 < dbP.updatePerson(r, oldPhone)) {
 			return true;
 		}
 		else {
@@ -54,11 +70,59 @@ public class PersonCtr {
 		}
 	}
 	
-	public boolean deletePerson(String phoneno) {
-		if(0 < dbP.deletePerson(phoneno)) {
+	public boolean updateStaff(String fname, String lname, String email,
+			String phoneno, String zipcode, String username, String password, String oldPhone) {
+		Staff s = new Staff(fname, lname, email, phoneno, zipcode, username, password);
+		
+		if(0 < dbP.updatePerson(s, oldPhone)) {
 			return true;
 		}
 		else {
+			return false;
+		}
+	}
+	
+	public boolean updateTeamLeader(String fname, String lname, String email,
+			String phoneno, String zipcode, String username, String password, String oldPhone) {
+		TeamLeader tl = new TeamLeader(fname, lname, email, phoneno, zipcode, username, password);
+		
+		if(0 < dbP.updatePerson(tl, oldPhone)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean updateManager(String fname, String lname, String email,
+			String phoneno, String zipcode, String username, String password,
+			double salary, String oldPhone) {
+		Manager m = new Manager(fname, lname, email, phoneno, zipcode, username, password, salary);
+		
+		if(0 < dbP.updatePerson(m, oldPhone)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean updatePlayer(String fname, String lname, String email,
+			String phoneno, String zipcode, String sbDay, String position,
+			String oldPhone) {
+		Player p = new Player(fname, lname, email, phoneno, zipcode, sbDay,
+				position);
+		if (0 < dbP.updatePerson(p, oldPhone)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean deletePerson(String phoneno) {
+		if (0 < dbP.deletePerson(phoneno)) {
+			return true;
+		} else {
 			return false;
 		}
 	}
