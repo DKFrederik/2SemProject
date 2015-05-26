9891b0980183b17151b5ba6619f8aa6957eac678
Nichlas Lindorf Pedersen <Nichlaslp@fastmail.com>
2015-05-26 10:57:09 +0200

    Interface for the DBAppointment class

---
+++ 2SemProject/src/dbLayer/IFAppointment.java
@@ -0,0 +1,42 @@
+package dbLayer;
+
+import java.util.ArrayList;
+
+import modelLayer.Appointment;
+
+public interface IFAppointment {
+
+	/**
+	 * Retrieves all Persons from the Person table in the db.
+	 * 
+	 * @param retriveAssociation
+	 *            Determines if associations should be retrieved or not.
+	 * @return An ArrayList of Person objects.
+	 */
+	public abstract ArrayList<Appointment> getAllAppointments(int id,
+			boolean retriveAssociation);
+
+	/**
+	 * Finds an appointment using the id. 
+	 * @param The
+	 *            date of the schedule to fetch
+	 * @return The schedule of the day.
+	 */
+	public abstract Appointment findAppointment(int id);
+
+	/**
+	 * Inserts an appoinment into the db. 
+	 * @param a the Appointment.
+	 * @param scheduleId the id of the schedule.
+	 * @return An int corresponding to the amount of changed rows or -1 if failed.
+	 */
+	public abstract int insertAppointment(Appointment a, int scheduleId);
+
+	/**
+	 * Deletes an appointment from the DB.
+	 * @param scheduleId the scheduleId for the appointments
+	 * @return An int corresponding to the amount of changed rows or -1 if failed.
+	 */
+	public abstract int deleteAppointments(int scheduleId);
+
+}
