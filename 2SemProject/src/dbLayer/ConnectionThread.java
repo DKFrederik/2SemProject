8fc6077868741044f9cf19a211b3b06b48e2fcb2
clausjoergensen91 <clausjoergensen91@gmail.com>
2015-05-29 12:33:31 +0200

    ConnectionThread changed from Runnable to extending Thread-class

--- 2SemProject/src/dbLayer/ConnectionThread.java
+++ 2SemProject/src/dbLayer/ConnectionThread.java
@@ -1,26 +1,24 @@
 package dbLayer;
 
-import dbLayer.*;
-
 public class ConnectionThread extends Thread {
-	
-	public static void main(String[] args) throws InterruptedException{
+
+	public static void main(String[] args) throws InterruptedException {
 		ConnectionThread ct1 = new ConnectionThread();
-		ct1.start();
+			ct1.start();
 	}
-	
+
 	@Override
 	public void run() {
-		try{
-		while(DBConnection.getInstance() != null){
-			System.out.println("Connected to " + DBConnection.databaseName);
-			Thread.sleep(3000);		
-		}
-			}catch (InterruptedException e) {
-				e.printStackTrace();
+		String Str = new String(DBConnection.databaseName);
+		try {
+			while (DBConnection.getInstance() != null) {
+				System.out.println("Connected to database: " + Str.substring(14, 17));
+				Thread.sleep(3000);
 			}
-		
-	
+		} catch (InterruptedException e) {
+				System.out.println("Lost connection to database: " + Str.substring(14,17));
+		}
+
 	}
 
 }
